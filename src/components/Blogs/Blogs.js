078bdcfbd2b148cc69a06eb18b56c23ce5e43ac9
moshiur25524 <moshiur.rahman25524@gmail.com>
2022-04-04 21:11:44 +0600

    Home component Done

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -4,10 +4,16 @@ import './Blogs.css'
 const Blogs = () => {
     return (
         <div>
+            {/* First Question */}
             <h1>What is Context API and what is the purpose of it?</h1>
+
             <p>Context API is the easiest way to sent data from parent to child components. Basically it is used in a Nesting Component tree. It rape around those items whom will be the receiver of props from their parents. Context API simplify the way of sending of props from parent to childs. To use Context API, firstly a context API have to be Created with export . Then a context.Provider rape around those components who will receive the data.</p>
+
+            {/* Second Question */}
             <h1>What is Semantic Tag?</h1>
+
             <p>Semantic tags are the meaningful Tag in HTML. It is inclueded in HTML 5. some non semantic tags don't have their own meaning about their content. Among them Div and span tags are used as a non semantic tag. Some Example of Semantic tags are Table tag, section tag, blog tag, address tag etc. Those Semantic can be read easily by developers and browsers. semantic tags are mostly used to arrange code in a code editor.</p>
+
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-            <h1>This is Home Page</h1>
+        <div className='home'>
+            <div>
+                <h1>Enjoy Time With Your Loving Car</h1>
+                <p>
+Digitization, increasing automation, and new business models have revolutionized other industries, and automotive will be no exception. These forces are giving rise to four disruptive technology-driven trends in the automotive sector: diverse mobility, autonomous driving, electrification, and connectivity.</p>
+            </div>
+            <div className='car-img'>
+                <img src={`https://img.freepik.com/free-photo/red-luxury-sedan-road_114579-5079.jpg?w=996&t=st=1649084557~exp=1649085157~hmac=85cae44df1a8b7ac225a13e9513a3209c37861cb8c075bf53f6ecdadfab8bb65`} alt="" />
+            </div>
         </div>
     );
 };
