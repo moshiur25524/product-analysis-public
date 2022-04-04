87690a8bab78e4b039237f8995422318a03ab1ba
moshiur25524 <moshiur.rahman25524@gmail.com>
2022-04-04 17:55:11 +0600

    blog component done

--- src/components/CustomLink/CustomLink.js
+++ src/components/CustomLink/CustomLink.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
 
-const CustomLink = () => {
+function CustomLink({ children, to, ...props }) {
+    let resolved = useResolvedPath(to);
+    let match = useMatch({ path: resolved.pathname, end: true });
+  
     return (
-        <div>
-            
-        </div>
+      <div>
+        <Link
+          style={{ color: match ? "green" : "black" }}
+          to={to}
+          {...props}
+        >
+        {children}
+        </Link>
+        
+      </div>
     );
-};
+  }
 
 export default CustomLink;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
 import './Header.css'
 
 const Header = () => {
     return (
         <div className='header'>
-            <Link to='/'>Home</Link>
-            <Link to='/review'>Review</Link>
-            <Link to='/dashboard'>DashBoard</Link>
-            <Link to='/blogs'>Blogs</Link>
-            <Link to='/about'>About</Link>
+            <CustomLink to='/'>Home</CustomLink>
+            <CustomLink to='/review'>Review</CustomLink>
+            <CustomLink to='/dashboard'>DashBoard</CustomLink>
+            <CustomLink to='/blogs'>Blogs</CustomLink>
+            <CustomLink to='/about'>About</CustomLink>
         </div>
     );
 };
