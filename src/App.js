328e731994b9170380faaa04dc91f81e069106a4
moshiur25524 <moshiur.rahman25524@gmail.com>
2022-04-04 17:03:38 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home'
+import Header from './components/Header/Header'
+import Review from './components/Review/Review'
+import DashBoard from './components/DashBoard/DashBoard'
+import Blogs from './components/Blogs/Blogs'
+import About from './components/About/About'
+import NotFound from './components/DashBoard/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/review' element={<Review></Review>}></Route>
+        <Route path='/dashboard' element={<DashBoard></DashBoard>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/DashBoard/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>Opps!!!</h1>
+            <h2>This is Unknown Place To Search</h2>
+            <h3>404 </h3>
+        </div>
+    );
+};
+
+export default NotFound;
